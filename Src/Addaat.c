0ebdaa77748fbc620a8f797a3a9d2e343026287b
Omar El Sayyed <omar@nomone.com>
2022-08-11 18:52:13 +0200

    - Started working on code generation.

--- Src/Addaat.c
+++ Src/Addaat.c
@@ -7,6 +7,104 @@
 
 #define PRINT_TREES 1
 #define PRINT_COLORED_TREES 1
+#define COLORIZE_CODE 1
+
+struct CodeGenerationData {
+    struct NString outString;
+    struct NVector colorStack; // const char*
+    const char* lastUsedColor;
+    int32_t indentationCount;
+};
+
+static void initializeCodeGenerationData(struct CodeGenerationData* codeGenerationData) {
+    NString.initialize(&codeGenerationData->outString, "");
+    NVector.initialize(&codeGenerationData->colorStack, 0, sizeof(const char*));
+    codeGenerationData->lastUsedColor = 0;
+    codeGenerationData->indentationCount = 0;
+}
+
+static void destroyCodeGenerationData(struct CodeGenerationData* codeGenerationData) {
+    NString.destroy(&codeGenerationData->outString);
+    NVector.destroy(&codeGenerationData->colorStack);
+}
+
+static void codeAppend(struct CodeGenerationData* codeGenerationData, const char* text) {
+
+    // Append indentation,
+    if (NCString.endsWith(NString.get(&codeGenerationData->outString), "\n")) {
+        for (int32_t i=0; i<codeGenerationData->indentationCount; i++) {
+            NString.append(&codeGenerationData->outString, "   ");
+        }
+    }
+
+    // Add color,
+    if (COLORIZE_CODE) {
+        if (!(NCString.equals(text, " ") || NCString.equals(text, "\n"))) {
+            const char* color;
+            if (NVector.size(&codeGenerationData->colorStack)) {
+                color = *(const char**) NVector.getLast(&codeGenerationData->colorStack);
+            } else {
+                color = NTCOLOR(STREAM_DEFAULT);
+            }
+            // Print color only if it's different from last color used,
+            if (color != codeGenerationData->lastUsedColor) {
+                NString.append(&codeGenerationData->outString, "%s", color);
+                codeGenerationData->lastUsedColor = color;
+            }
+        }
+    }
+
+    // Append text,
+    NString.append(&codeGenerationData->outString, "%s", text);
+}
+
+static void generateCodeImplementation(struct NCC_ASTNode* tree, struct CodeGenerationData* codeGenerationData) {
+    // Moved the implementation to a separate function to remove the codeGenerationData from the interface.
+
+    const char* ruleNameCString = NString.get(&tree->name);
+
+    boolean proceedToChildren = False;
+    if (NCString.equals(ruleNameCString, "insert space")) {
+        codeAppend(codeGenerationData, " ");
+    } else if (NCString.equals(ruleNameCString, "insert \n")) {
+        if (!NCString.endsWith(NString.get(&codeGenerationData->outString), "\n")) codeAppend(codeGenerationData, "\n");
+    } else if (NCString.equals(ruleNameCString, "insert \ns")) {
+        codeAppend(codeGenerationData, "\n");
+    } else if (NCString.equals(ruleNameCString, "OB")) {
+        codeAppend(codeGenerationData, "{");
+        codeGenerationData->indentationCount++;
+    } else if (NCString.equals(ruleNameCString, "CB")) {
+        codeGenerationData->indentationCount--;
+        codeAppend(codeGenerationData, "}");
+    } else if (NCString.equals(ruleNameCString, "line-cont")) {
+        codeAppend(codeGenerationData, " \\\n");
+    } else if (NCString.equals(ruleNameCString, "line-comment") || NCString.equals(ruleNameCString, "block-comment")) {
+        NVector.pushBack(&codeGenerationData->colorStack, &NTCOLOR(BLACK_BRIGHT));
+        codeAppend(codeGenerationData, NString.get(&tree->value));
+        const char *color; NVector.popBack(&codeGenerationData->colorStack, &color);
+    } else {
+        if (NVector.size(&tree->childNodes)) {
+            proceedToChildren = True;
+        } else {
+            // Leaf node,
+            codeAppend(codeGenerationData, NString.get(&tree->value));
+        }
+    }
+
+    if (!proceedToChildren) return;
+    // Not a leaf, print children,
+    int32_t childrenCount = NVector.size(&tree->childNodes);
+    for (int32_t i=0; i<childrenCount; i++) generateCodeImplementation(*((struct NCC_ASTNode**) NVector.get(&tree->childNodes, i)), codeGenerationData);
+}
+
+static void generateCode(struct NCC_ASTNode* tree, struct NString* outString) {
+
+    struct CodeGenerationData codeGenerationData;
+    initializeCodeGenerationData(&codeGenerationData);
+    generateCodeImplementation(tree, &codeGenerationData);
+    NString.set(outString, "%s", NString.get(&codeGenerationData.outString));
+    destroyCodeGenerationData(&codeGenerationData);
+}
 
 static void test(struct NCC* ncc, const char* code) {
 
@@ -23,6 +121,11 @@ static void test(struct NCC* ncc, const char* code) {
         NCC_ASTTreeToString(tree.node, 0, &treeString, PRINT_COLORED_TREES /* should check isatty() */);
         NLOGI(0, "%s", NString.get(&treeString));
         #endif
+
+        // Generate code,
+        NString.set(&treeString, "");
+        generateCode(tree.node, &treeString);
+        NLOGI(0, "%s", NString.get(&treeString));
 
         // Cleanup,
         NString.destroy(&treeString);
